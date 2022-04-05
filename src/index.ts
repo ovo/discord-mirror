04bdc0424edc289f63e4baf98a558621e17e772f
peyton <peyton.arbuckle@gmail.com>
2022-04-05 09:04:23 -0500

    v2 init swaaaag

--- src/index.ts
+++
@@ -1,8 +0,0 @@
-import fs from 'fs';
-import { listen, createServer, getChannels } from './modules/discord';
-
-if (fs.existsSync('./map.json')) {
-  listen();
-} else {
-  getChannels().then((channels) => createServer(channels)).then(() => listen());
-}
