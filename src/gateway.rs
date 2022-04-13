348535dad7483646657fba202d54bc5b9bf93831
peyton <peyton.arbuckle@gmail.com>
2022-04-13 13:21:20 -0500

    working gateway

--- src/gateway.rs
+++ src/gateway.rs
@@ -1,6 +1,8 @@
-use serde::{Serialize, Deserialize};
+use serde::{Deserialize, Serialize};
 use serde_json::json;
 use std::net::TcpStream;
+use std::thread;
+use std::time::{Duration, Instant};
 use websocket::client::{sync::Client, ClientBuilder};
 use websocket::header::{extensions::Extension, Headers, WebSocketVersion};
 use websocket::native_tls::TlsStream;
@@ -9,24 +11,72 @@ use websocket::{Message, OwnedMessage, WebSocketError};
 const SOCKET_URL: &'static str = "wss://gateway.discord.gg/?encoding=json&v=9";
 const USER_AGENT: &'static str = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.4 Safari/605.1.15";
 
-
 #[derive(Serialize, Deserialize, Debug)]
 struct Payload {
     op: i32,
     s: Option<i32>,
     t: Option<String>,
-    d: serde_json::Value
+    d: serde_json::Value,
 }
 
+#[derive(Serialize, Deserialize, Debug)]
+struct Heartbeat {
+    heartbeat_interval: i64,
 
-fn generate_socket_key() -> [u8; 16] {
-    let mut bytes = [0u8; 16];
+    #[serde(rename = "_trace")]
+    trace: Vec<String>,
+}
 
-    for i in 0..16 {
-        bytes[i] = rand::random::<u8>();
-    }
+#[derive(Serialize, Deserialize, Debug)]
+struct MessageEvent {
+    #[serde(rename = "type")]
+    d_type: i64,
+    tts: bool,
+    timestamp: String,
+    referenced_message: Option<serde_json::Value>,
+    pinned: bool,
+    nonce: String,
+    mentions: Vec<Option<serde_json::Value>>,
+    mention_roles: Vec<Option<serde_json::Value>>,
+    mention_everyone: bool,
+    member: Member,
+    id: String,
+    flags: i64,
+    embeds: Vec<Option<serde_json::Value>>,
+    edited_timestamp: Option<serde_json::Value>,
+    content: String,
+    components: Vec<Option<serde_json::Value>>,
+    channel_id: String,
+    author: Author,
+    attachments: Vec<Option<serde_json::Value>>,
+    guild_id: String,
+}
 
-    bytes
+#[derive(Serialize, Deserialize, Debug)]
+struct Author {
+    username: String,
+    public_flags: i64,
+    id: String,
+    discriminator: String,
+    avatar: Option<serde_json::Value>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Member {
+    roles: Vec<Option<serde_json::Value>>,
+    mute: bool,
+    joined_at: String,
+    hoisted_role: Option<serde_json::Value>,
+    flags: i64,
+    deaf: bool,
+}
+
+pub struct Gateway {
+    token: String,
+    server_id: String,
+    heartbeat_time: Instant,
+    client: Client<TlsStream<TcpStream>>,
+    heartbeat_interval: i32,
 }
 
 fn get_headers() -> Headers {
@@ -44,7 +94,17 @@ fn get_headers() -> Headers {
     headers
 }
 
-pub fn create_client() -> Result<Client<TlsStream<TcpStream>>, WebSocketError> {
+fn generate_socket_key() -> [u8; 16] {
+    let mut bytes = [0u8; 16];
+
+    for i in 0..16 {
+        bytes[i] = rand::random::<u8>();
+    }
+
+    bytes
+}
+
+fn new_client() -> Result<Client<TlsStream<TcpStream>>, WebSocketError> {
     ClientBuilder::new(SOCKET_URL)
         .unwrap()
         .add_extension(Extension {
@@ -63,44 +123,137 @@ pub fn create_client() -> Result<Client<TlsStream<TcpStream>>, WebSocketError> {
         .connect_secure(None)
 }
 
-pub fn listen(mut client: Client<TlsStream<TcpStream>>) {
-    loop {
-        let message = match client.recv_message() {
-            Ok(msg) => msg,
-            Err(e) => {
-                println!("{:?}", e);
-                if let Err(e) = client.shutdown() {
-                    println!("{:?}", e);
-                }
-                break;
-            }
-        };
+impl Gateway {
+    pub fn new(token: String, server_id: String) -> Self {
+        let client = new_client()
+            .map_err(|e| println!("Could not connect to socket: {}", e))
+            .unwrap();
 
-        println!("{:?}", message);
+        Self {
+            token,
+            server_id,
+            heartbeat_time: Instant::now(),
+            client,
+            heartbeat_interval: 41250,
+        }
+    }
+
+    fn authenticate(&mut self) {
+        let token = &self.token;
+        let client = &mut self.client;
+
+        let msg = json!({
+          "op": 2,
+          "d": {
+            "token": token,
+            "capabilities": 253,
+            "properties": {
+              "os": "Mac OS X",
+              "browser": "Safari",
+              "device": "",
+              "system_locale": "en-US",
+              "browser_user_agent": USER_AGENT,
+              "browser_version": "15.4",
+              "os_version": "10.15.7",
+              "referrer": "https://www.google.com/",
+              "referring_domain": "www.google.com",
+              "search_engine": "google",
+              "referrer_current": "",
+              "referring_domain_current": "",
+              "release_channel": "stable",
+              "client_build_number": 123680,
+              "client_event_source": null
+            },
+            "presence": {
+              "status": "online",
+              "since": 0,
+              "activities": [],
+              "afk": false
+            },
+            "compress": false,
+            "client_state": {
+              "guild_hashes": {},
+              "highest_last_message_id": "0",
+              "read_state_version": 0,
+              "user_guild_settings_version": -1,
+              "user_settings_version": -1
+            }
+          }
+        });
+
+        client
+            .send_message(&Message::text(msg.to_string()))
+            .map_err(|e| eprintln!("Error authenticating: {:?}", e))
+            .unwrap();
+    }
+
+    pub fn listen(&mut self) {
+        loop {
+            let client = &mut self.client;
+            let message = match client.recv_message() {
+                Ok(msg) => msg,
+                Err(e) => {
+                    println!("{:?}", e);
+                    if let Err(e) = client.shutdown() {
+                        println!("{:?}", e);
+                    }
+                    break;
+                }
+            };
+
+            self.message_event(message);
+
+        }
+    }
+
+    fn message_event(&mut self, message: OwnedMessage) {
+        let client = &mut self.client;
 
         match message {
             OwnedMessage::Close(_) => {
                 println!("Received close message - resetting connection");
-                let new_client = create_client().unwrap();
-                listen(new_client);
+                return;
             }
             OwnedMessage::Ping(data) => {
                 println!("Received ping message");
-                client.send_message(&OwnedMessage::Pong(data)).unwrap();
+                client
+                    .send_message(&OwnedMessage::Pong(data.to_vec()))
+                    .unwrap();
             }
             OwnedMessage::Pong(_) => {
                 println!("Received pong message");
             }
             OwnedMessage::Text(text) => {
-                println!("Received text message: {}", text);
                 let deserialized_msg: Payload = serde_json::from_str(&text).unwrap();
 
                 match deserialized_msg.op {
-                    10 => authenticate(&mut client, std::env::var("USER_DISCORD_TOKEN").expect("Expected user token in env")),
-                    _ => ()
+                    10 => {
+                        // Authenticate and set heartbeat interval;
+                        self.authenticate();
+                    }
+                    0 => {
+                        /*
+                        match deserialized_msg.t {
+                            "MESSSAGE_CREATE" =>
+                        }
+                        */
+                        // Check if t is MESSAGE_CREATE:
+                        if let Some(t) = deserialized_msg.t {
+                            if t == "MESSAGE_CREATE" {
+                                let message_event: MessageEvent =
+                                    serde_json::from_value(deserialized_msg.d).unwrap();
+                                println!(
+                                    "{:?}#{:?} in {:?}: {:?}",
+                                    message_event.author.username,
+                                    message_event.author.discriminator,
+                                    message_event.channel_id,
+                                    message_event.content
+                                );
+                            }
+                        }
+                    }
+                    _ => println!("{}\n{:?}\n", deserialized_msg.op, deserialized_msg),
                 }
-                println!("{}", deserialized_msg.op);
-                
             }
             OwnedMessage::Binary(data) => {
                 println!("Received binary message: {:?}", data);
@@ -109,47 +262,15 @@ pub fn listen(mut client: Client<TlsStream<TcpStream>>) {
     }
 }
 
-fn authenticate(client: &mut Client<TlsStream<TcpStream>>, token: String) {
-    let msg = json!({
-      "op": 2,
-      "d": {
-        "token": token,
-        "capabilities": 253,
-        "properties": {
-          "os": "Mac OS X",
-          "browser": "Safari",
-          "device": "",
-          "system_locale": "en-US",
-          "browser_user_agent": USER_AGENT,
-          "browser_version": "15.4",
-          "os_version": "10.15.7",
-          "referrer": "https://www.google.com/",
-          "referring_domain": "www.google.com",
-          "search_engine": "google",
-          "referrer_current": "",
-          "referring_domain_current": "",
-          "release_channel": "stable",
-          "client_build_number": 123680,
-          "client_event_source": null
-        },
-        "presence": {
-          "status": "online",
-          "since": 0,
-          "activities": [],
-          "afk": false
-        },
-        "compress": false,
-        "client_state": {
-          "guild_hashes": {},
-          "highest_last_message_id": "0",
-          "read_state_version": 0,
-          "user_guild_settings_version": -1,
-          "user_settings_version": -1
-        }
-      }
-    });
-
-    client.send_message(&Message::text(msg.to_string()))
-        .map_err(|e| eprintln!("Error authenticating: {:?}", e))
-        .unwrap();
+fn heartbeat(client: &mut Client<TlsStream<TcpStream>>, count: i32) {
+    loop {
+        let msg = json!({
+            "op": 1,
+            "d": count,
+        });
+        client
+            .send_message(&OwnedMessage::Text(msg.to_string()))
+            .unwrap();
+        thread::sleep(Duration::from_millis(count as u64));
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,17 @@ mod gateway;
 mod mirror;
 
 use dotenv::dotenv;
+use tokio::runtime::Builder;
 use std::env;
 
 #[tokio::main]
 async fn main() {
     dotenv().ok();
-
-    env::var("USER_DISCORD_TOKEN").expect("Expected user token in env");
+    let user_token = env::var("USER_DISCORD_TOKEN").expect("Expected user token in env");
+    let server_id = env::var("TARGET_SERVER_ID").expect("Expected target id in env");
     env::var("BOT_DISCORD_TOKEN").expect("Expected bot token in env");
-    env::var("TARGET_SERVER_ID").expect("Expected target id in env");
 
-    let client = gateway::create_client()
-        .map_err(|e| eprintln!("Error connecting: {}", e))
-        .unwrap();
-
-    gateway::listen(client);
+    let mut client = gateway::Gateway::new(user_token, server_id);
+    client.listen();
 
 }
